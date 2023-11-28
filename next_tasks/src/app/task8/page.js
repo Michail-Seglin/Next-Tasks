3337aa23cc8b58f6f7ac3e8782b193f7225832e8
Mikhail <mihailsavuskin13@gmail.com>
2023-11-28 21:36:49 +0300

    task_8

--- next_tasks/src/app/task8/page.js
+++ next_tasks/src/app/task8/page.js
@@ -5,12 +5,24 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 export default function Task8() {
+    const [value, setValue] = useState('');
+    async function getIp() {
+        const res = await axios.get(' https://api.ipify.org/?format=json')
+        setValue(res.data.ip);
+    }
+
+    useEffect(() => {
+        getIp();
+    }, []);
+
     return (
         <>
+
             <p>Создайте компонент, который выполняет запрос к API
                 https://api.ipify.org/?format=json и отображает полученные данные в заголовок.
                 (решить задачу через клиентский компонент)
             </p>
+            <p>{value}</p>
         </>
     )
 }
