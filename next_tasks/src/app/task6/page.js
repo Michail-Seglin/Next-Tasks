3955293f55e5a24e252a04af05302f3d8e5149d8
MichailSehlin <michailsavuskin13@gmail.com>
2023-11-28 14:51:07 +0300

    app_6

--- next_tasks/src/app/task6/page.js
+++ next_tasks/src/app/task6/page.js
@@ -1,3 +1,5 @@
+'use client'
+
 import style from './style.module.css'
 import { useState } from 'react'
 
@@ -6,12 +8,14 @@ export default function Task6() {
 
     return (
 
-        <>
-            <p> Создайте кнопку, которая позволяет пользователю переключаться между светлой
+        <div className={bool ? style.light : style.dark}>
+            <p className={bool ? style.lightP : style.darkP}> Создайте кнопку, которая позволяет пользователю переключаться между светлой
                 и темной темой интерфейса. Используйте useState, чтобы отслеживать текущую
                 тему. Применяйте соответствующие стили в зависимости от выбранной темы.</p>
 
-
-        </>
+            <button onClick={() => setBool(!bool) }>
+                {bool ? 'dark' : 'light'}
+            </button>
+        </div>
     )
 }
