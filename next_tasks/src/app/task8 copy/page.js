5c788d3ff6cb9787b7e5df8941176bd8ceaaa7fa
Michail-Seglin <michailsavuskin13@gmail.com>
2023-11-28 15:23:45 +0300

    app_n

---
+++ next_tasks/src/app/task8 copy/page.js
@@ -0,0 +1,16 @@
+'use client'
+
+import axios from 'axios';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+export default function Task8() {
+    return (
+        <>
+            <p>Создайте компонент, который выполняет запрос к API
+                https://api.ipify.org/?format=json и отображает полученные данные в заголовок.
+                (решить задачу через клиентский компонент)
+            </p>
+        </>
+    )
+}
