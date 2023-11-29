a187ac4c9dfbd8aec98ecaf17e40faea4d9e416c
Mikhail <mihailsavuskin13@gmail.com>
2023-11-29 19:17:31 +0300

    task_10

--- next_tasks/src/app/task10/page.js
+++ next_tasks/src/app/task10/page.js
@@ -1,9 +1,28 @@
+'use client'
+import { useState, useMemo } from "react"
+
 export default function Task10() {
+    const [number, setNumber] = useState();
+    const [value, setValue] = useState();
+
+    function factorial(n) {
+        return n ? n * factorial(n - 1) : 1;
+    }
+
+    useMemo(() => {
+        setValue(factorial(number));
+
+    }, [number]);
+
+    console.log(number);
+
     return (
         <>
             <p>Создайте компонент, в котором пользователь может ввести число. Используйте
                 useMemo, чтобы вычислить факториал введенного числа. Выведите результат
                 факториала на странице.</p>
+            <input onChange={(e) => setNumber(e.target.value)}></input>            <p>{number}</p>
+            <p>{number}</p>
         </>
     )
 }
--- next_tasks/src/app/task9/page.js
+++ next_tasks/src/app/task9/page.js
@@ -1,9 +1,17 @@
-export default function Task9() {
+import axios from "axios";
+
+export default async function Task9() {
+    const getIp = async () => {
+        const res = await axios.get('https://api.ipify.org/?format=json')
+
+        return res.data.ip;
+    }
     return (
         <>
             <p>Создайте компонент, который выполняет запрос к API
                 https://api.ipify.org/?format=json и отображает полученные данные в заголовок.
                 (решить задачу через серверный компонент)</p>
+            <p>{await getIp()}</p>
         </>
     )
 }
