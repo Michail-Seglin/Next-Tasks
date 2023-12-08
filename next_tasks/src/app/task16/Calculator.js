c5746381de34b5b3c8f1932252bcd739a9ad9103
Mikhail <mihailsavuskin13@gmail.com>
2023-12-08 22:37:58 +0300

    add_16_new

--- next_tasks/src/app/task16/Calculator.js
+++ next_tasks/src/app/task16/Calculator.js
@@ -1,16 +1,20 @@
 export default function Calculator() {
 
     function sum() {
+        <input type="button" value={'+'} onClick={e => setValue(value + e.target.value)} />
 
     }
 
     function subtraction() {
+        <input type="button" value={'-'} onClick={e => setValue(value + e.target.value)} />
 
     }
 
-    function multiplication() { }
+    function multiplication() {            <input type="button" value={'*'} onClick={e => setValue(value + e.target.value)} />
+}
 
-    function division() { }
+    function division() {             <input type="button" value={'/'} onClick={e => setValue(value + e.target.value)} />
+}
 
     return (
         <>
